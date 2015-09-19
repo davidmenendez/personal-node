3b10584e80828060a10c15f2daf51f9eecc7e45a
davidmenendez <damenend@gmail.com>
2015-09-18 21:18:16 -0500

    almost done with the structure

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var routes = require('./routes/index');
+app.use('/', routes);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.set('port', process.env.PORT || 8000);
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', function (req, res) {
-	res.send('Hello World!');
-});
 
 app.listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
