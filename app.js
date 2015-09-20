d6ec8490c6e8de4954724cfba37a7d849c9dab74
davidmenendez <damenend@gmail.com>
2015-09-20 18:22:07 -0500

    added browsersync for better development

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var compression = require('compression')
 var routes = require('./routes/index');
+var logger = require('morgan');
 var app = express();
 
 app.use(compression());
@@ -11,6 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.set('port', process.env.PORT || 8000);
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function(){
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var nodemon = require('gulp-nodemon');
 
 gulp.task('styles', function() {
 	gulp.src('src/sass/**/styles.scss')
 	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('public/css/'));
+	.pipe(gulp.dest('public/css/'))
+	.pipe(browserSync.stream());
 });
 
-gulp.task('watch', function(){
-	gulp.watch('src/sass/**/*.scss',['styles']);
+gulp.task('serve', ['styles', 'nodemon'], function() {
+	browserSync.init(null, {
+		proxy: 'http://localhost:8000',
+		files: ['public/**/*.*'],
+		port: 8001,
+	});
+	gulp.watch('./src/sass/**/*.scss',['styles']);
+	gulp.watch('views/*.jade').on('change', browserSync.reload);
 });
 
-gulp.task('default',['watch', 'styles']);
+gulp.task('nodemon', function (cb) {
+	var started = false;
+	return nodemon({
+		script: 'app.js'
+	}).on('start', function () {
+		if (!started) {
+			cb();
+			started = true; 
+		} 
+	});
+});
+
+gulp.task('default', ['serve']);
