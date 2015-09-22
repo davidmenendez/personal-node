74e7855807cbc9bf6fce5803f3a16a81b5419684
davidmenendez <damenend@gmail.com>
2015-09-21 20:23:23 -0500

    added the figure highlight stuff

--- routes/index.js
+++ routes/index.js
@@ -3,44 +3,44 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'David Menendez | Front End Developer' });
+	res.render('index', { title: 'Front End Developer' });
 });
 router.get('/portfolio', function(req, res, next) {
-	res.render('portfolio', { title: 'David Menendez | Portfolio' });
+	res.render('portfolio', { title: 'Portfolio' });
 });
 
 
 router.get('/portfolio/hadassah', function(req, res, next) {
-	res.render('portfolio/hadassah', { title: 'David Menendez | Hadassah' });
+	res.render('portfolio/hadassah', { title: 'Hadassah' });
 });
 router.get('/portfolio/colorado-childrens-hospital', function(req, res, next) {
-	res.render('portfolio/colorado-childrens-hospital', { title: 'David Menendez | Colorado Childrens Hospital' });
+	res.render('portfolio/colorado-childrens-hospital', { title: 'Colorado Childrens Hospital' });
 });
 router.get('/portfolio/blackbaud', function(req, res, next) {
-	res.render('portfolio/blackbaud', { title: 'David Menendez | American Cancer Society' });
+	res.render('portfolio/blackbaud', { title: 'American Cancer Society' });
 });
 router.get('/portfolio/be-a-character', function(req, res, next) {
-	res.render('portfolio/be-a-character', { title: 'David Menendez | Be A Character' });
+	res.render('portfolio/be-a-character', { title: 'Be A Character' });
 });
 router.get('/portfolio/brigewater', function(req, res, next) {
-	res.render('portfolio/brigewater', { title: 'David Menendez | Bridgwater' });
+	res.render('portfolio/brigewater', { title: 'Bridgwater' });
 });
 router.get('/portfolio/national-hamburger-month', function(req, res, next) {
-	res.render('portfolio/national-hamburger-month', { title: 'David Menendez | National Hamburger Month' });
+	res.render('portfolio/national-hamburger-month', { title: 'National Hamburger Month' });
 });
 router.get('/portfolio/zimmerman-blog', function(req, res, next) {
-	res.render('portfolio/zimmerman-blog', { title: 'David Menendez | Zimmerman Blog' });
+	res.render('portfolio/zimmerman-blog', { title: 'Zimmerman Blog' });
 });
 router.get('/portfolio/bikestreet', function(req, res, next) {
-	res.render('portfolio/bikestreet', { title: 'David Menendez | Bikestreet' });
+	res.render('portfolio/bikestreet', { title: 'Bikestreet' });
 });
 router.get('/portfolio/hottest-halloween', function(req, res, next) {
-	res.render('portfolio/hottest-halloween', { title: 'David Menendez | Hottest Halloween' });
+	res.render('portfolio/hottest-halloween', { title: 'Hottest Halloween' });
 });
 
 
 router.get('/about', function(req, res, next) {
-	res.render('about', { title: 'David Menendez | About' });
+	res.render('about', { title: 'About' });
 });
 
 module.exports = router;
